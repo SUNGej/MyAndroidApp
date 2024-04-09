df08021a00dd418e6ccf2e040ecd3bc66fcdaea6
SUNGej <ulkenfkaus@gmail.com>
2024-04-09 12:30:56 +0900

    test for feature(select image file)

--- app/src/main/java/com/sung/myapplication/MainActivity.java
+++ app/src/main/java/com/sung/myapplication/MainActivity.java
@@ -1,14 +1,67 @@
 package com.sung.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonSelectImage;
+    ImageView imageViewSelectedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSelectImage = findViewById(R.id.buttonSelectImage);
+        imageViewSelectedImage = findViewById(R.id.imageViewSelectedImage);
+
+        buttonSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 101) {
+            if (resultCode == RESULT_OK) {
+                Uri fileUri = data.getData();
+
+                ContentResolver resolver = getContentResolver();
+
+                try {
+                    InputStream inputStream = resolver.openInputStream(fileUri);
+                    Bitmap imageBitmap = BitmapFactory.decodeStream(inputStream);
+                    imageViewSelectedImage.setImageBitmap(imageBitmap);
+
+                    inputStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(intent, 101);
     }
 }
